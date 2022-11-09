009f9da1cae1ca71eae953c5db8e8edf17c904b6
SpookDaCat <justismortenson@gmail.com>
2022-11-08 22:58:51 -0600

    css start?

--- index.php
+++ index.php
@@ -1,32 +1,47 @@
 <?php
 // home page
 session_start();
-require_once("SplitPageTemplate.php");
+require_once("Template.php");
 require_once("WebServiceClient.php");
 
-$template = new SplitPageTemplate("Home");
+$template = new Template("Home");
 
 $_SESSION['role'] = 'guest';
 
 
-print $template->beginHTML() . $template->openLeftPane();
+print $template->beginHTML();
 
-print "<h4>Testing</h4>";
+print "
+<div class=\"hazy\"> 
+    <div class=\"white-overlay align-items-center mt\">
+    <h4> Welcome! Sign in </h4>";
 
 if (isset($_SESSION['errors'])) {
     foreach ($_SESSION['errors'] as $e) {
         print "<p class=\"error\">$e</p>";
     }
 }
-
-print $template->closeLeftOpenRightPane();
-
 print "
-<form method=\"POST\" action=\"authentication.php\">
-    Username: <input name=\"username\" id=\"username\"/><br/>
-    Password: <input name=\"password\" id=\"password\"/><br/>
-    <input type=\"submit\" name=\"submitform\" value=\"Submit\">
-</form>
-<a href=\"dashboard.php\">Continue as Guest</a>";
-print $template->closeRightPane() . $template->closeHTML();
+        <div>
+            <form method=\"POST\" action=\"authentication.php\">
+                <div class=\"container\"> <div class=\"row\">
+                <div class=\"col-6 float-left\">
+                    <p>Username:</p>
+                    <p>Password: </p>
+                    <input type=\"submit\" name=\"submitform\" value=\"Submit\">
+                </div>
+                <div class=\"col-6\">
+                    <input name=\"username\" id=\"username\"/><br/>
+                    <input name=\"password\" id=\"password\"/><br/>
+                </div>
+
+                </div></div>
+            </form>
+            <a href=\"dashboard.php\">Continue as Guest</a> 
+        </div>
+    </div>
+</div>
+";
+
+print $template->closeHTML();
 ?>
