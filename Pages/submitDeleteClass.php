b77ff00c8767a7ce292b1ffe2ba7962a6711c810
SpookDaCat <jmort593@uwsp.edu>
2022-12-02 08:46:06 -0600

    students drop course before it deleted
    Co-Authored-By: Jmort593 <113048834+jmort593@users.noreply.github.com>

--- Pages/submitDeleteClass.php
+++ Pages/submitDeleteClass.php
@@ -1,4 +1,10 @@
 <?php
+
+    function deletionError() {
+        $_SESSION['errors'] = array("Error with deleting class");
+        die(header("Location: dashboard.php"));
+    }
+
     session_start();
 
     require_once(__DIR__.'/../WebServiceClient.php');
@@ -30,6 +36,45 @@
     // make the request to the api
     $url = "http://cnmt310.classconvo.com/classreg/";
     $client = new WebServiceClient($url);
+
+
+    
+    // remove students from couse to be deleted
+    
+    $postData = array("apikey" => APIKEY,
+                 "apihash" => APIHASH,
+                 "data" => array( "course_id" => $course_id ),
+                 "action" => "getstudentlistbycourse"
+                 );
+
+    $client->setPostFields($postData);
+    $json = (object) json_decode($client->send());
+
+    if ($json == null || !isset($json->result) || $json->result != "Success") { // might need more checks
+        deletionError();
+    } 
+
+    $students = $json->data;
+    if (!isset($student) || !is_array($students)) {
+        deletionError();
+    }
+
+    foreach ($students as $s) {
+        $postData = array("apikey" => APIKEY,
+                 "apihash" => APIHASH,
+                 "data" => array( "course_id" => $course_id, "student_id"=> $s->student_id ),
+                 "action" => "delstudentfromcourse"
+                 );
+
+        $client->setPostFields($postData);
+        $json = (object) json_decode($client->send());
+
+        if (!isset($json, $json->result) || $json->result != "Success") {
+            deletionError();
+        }
+    }
+
+
     
     $postData = array("apikey" => APIKEY,
                  "apihash" => APIHASH,
@@ -41,11 +86,10 @@
     $json = (object) json_decode($client->send());
 
     if ($json == null || !isset($json->result) || $json->result != "Success") { // might need more checks
-        $_SESSION['errors'] = array("Error with adding class", json_encode($json));
-        die(header("Location: addClass.php"));
+        deletionError();
     } else { // ik this not needed but since they are linked logically I like to have it
         unset($_SESSION['errors']);
-        $_SESSION['successes'] = array("Success adding a class!");
+        $_SESSION['successes'] = array("Success deleted a class!");
         die(header("Location: dashboard.php"));
     }       
 ?>
