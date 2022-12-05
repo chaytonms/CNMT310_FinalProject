b66baf55d9e4465fb72533fe0a7d4af687b7ad2a
Jmort593 <jmort593@uwsp.edu>
2022-12-05 14:25:43 -0600

    worked on file eventually be used to either add or remove students from courses

--- FormWizard.php
+++ FormWizard.php
@@ -14,6 +14,17 @@ class FormWizard {
     </div>\n";
     }
 
+    public function standardSubmit($fallback, $label="Submit") {
+        return "<div class=\"row m-0\">
+            <div class=\"col m-0 p-0\">
+                <input type=\"submit\" name=\"submitform\" value=\"$label\"><br/>
+            </div>
+            <div class=\"col m-0 p-0\">
+                <a href=\"$fallback\">BACK</a>
+            </div>
+        </div>";
+    }
+
 
 }
 
---
+++ Pages/fileUsedToEnsureNoConflicts.php
@@ -0,0 +1,32 @@
+<?php
+    session_start();
+
+    require_once("../FormWizard.php");
+    require_once("../Template.php");
+
+    $FW = new FormWizard();
+    $template = new Template("Add Class");
+
+    if (!isset($_POST, $_POST['course_id'], $_SESSION['user'], $_SESSION['user']->roles)) {
+        $_SESSION['errors'] = array("Session Error");
+        die(header("Location: dashboard.php"));
+    }
+
+    $course_id = $_POST['course_id'];
+
+	print $tem->beginHTML();
+	
+	print '
+	<h2>Manage Course</h2>
+	<form method="POST" action="confirmEnrollClass.php">
+        ' . $FW->standardInput("Student ID Number", "student_id", inputType:"number", classes:"") . '
+        <input type="hidden" name="course_id" value="'. $course_id .'/>
+		' . $FW->standardSubmit("dashboard.php", "Remove Student to Course") . ' />
+	</form>';
+	
+	print $tem->closeHTML();
+?>
+
+
+
+?>
